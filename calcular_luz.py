cda2e2e82dd94611ef0fb6a0fc8c47e99201ee3c
Antonio Mateo <91947321+fluctlights@users.noreply.github.com>
2022-05-25 20:20:06 +0200

    Estructurando el código

--- calcular_luz.py
+++ calcular_luz.py
@@ -1,104 +1,102 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+#from webbrowser import get
+from subprocess import call
 import requests
 import json
 import statistics
 
-#Creando el objeto JSON (cabeceras), que requiere un token personal y la URL que referencia la tarifa
-TOKEN = "b8cdeb86a9e01c367f4a2fcf5b76580088eab4677ce4cee21d0523489d896b4a" #Introducir el token personal que te da REE
-url = 'https://esios.ree.es/indicators/10229' #pagina que referencia a la PVPC 2.0A 
-headers = {'Accept':'application/json; application/vnd.esios-api-v2+json','Content-Type':'application/json','Host':'api.esios.ree.es','Authorization':'Token token=' + TOKEN}
-#Obtener respuesta de la API tras meter la cabecera. y la URL que tiene la tarifa
-response = requests.get(url, headers=headers)
+TOKEN = "b8cdeb86a9e01c367f4a2fcf5b76580088eab4677ce4cee21d0523489d896b4a" #Token de acceso
+ID_PENINSULA = 8741 #Para filtrar por los precios de la Peninusula
+URL = 'https://api.esios.ree.es/indicators/1001'
+CABECERAS = {'Accept':'application/json; application/vnd.esios-api-v2+json','Content-Type':'application/json','Host':'api.esios.ree.es','Authorization':'Token token=\"TOKEN\"'}
 
-#Si la respuesta es valida, continuamos
-if response.status_code == 200:
-
-    json_data = json.loads(response.text)
-    valores = json_data['indicator']['values']
-    #print(response.json())
-
-    precios = [x['value'] for x in valores]
+def calcular_luz(datos_json):
     
-    hora = 0
-    for precio in precios:
-        print("%s horas - %s €" %(str(hora).zfill(2), str(round(precio/1000, 4))))
-        hora += 1
-    
-    valor_min = min(precios)
-    valor_max = max(precios)
-    valor_med = round(statistics.mean(precios),2)
-    
-    print("Precio mínimo: %s" % str(valor_min/1000))
-    print("Precio máximo: %s" % str(valor_max/1000))
-    print("Precio medio: %s" % str(valor_med/1000))
+        #Para filtrar por valores, y solo aquellos que sean de la Peninsula
+        valores = datos_json['indicator']['values']
+        valores_peninsula = [x for x in valores if x['geo_id'] == ID_PENINSULA]
 
-    '''
-    json_data = json.loads(response.text) #decodificamos la respuesta de la API
-    valores = json_data['indicator']['values'] #Formateamos la array de arriba, donde tenemos [ID, valor]
-    precios = [x['value'] for x in valores] #crear array de 24 filas, cada hora, un precio.
-    buena_hora = [False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False] #boolean que recoge si el precio es bueno o no, segun la lista precios
-    puntero = 0 #para rellenar las horas
-    dif = 0 #si la diferencia es pequeña entre horas contiguas, entonces tambien entrara la hora conflictiva en las horas validas
-    valor_med = round(statistics.fmean(precios),2) #media del precio de electricidad del dia 
-    valor_valido_sup = valor_med + 0.005  #limite que marco yo mismo de lo que es razonabl
-    intervalo = [] #para generar los intervalos validos 
-    encendido_apagado = [] #para marcar los horarios concretos en una lista
+        #Recogiendo precios
+        precios = [x['value'] for x in valores_peninsula]
+        return precios
 
-    for precio in precios:
-        if precio < valor_valido_sup:
-            buena_hora[puntero] = True
+def obtener_precios(datos_json, valor): #cada dia, este hilo se activara para leer los valores nuevos
+
+    response = requests.get(URL, headers=CABECERAS)
+
+    if response.status_code == 200:  #conexion correcta
+        datos_json = json.loads(response.text) #almacenamos respuesta en formato JSON
+        return datos_json
+    
+    else: return datos_json
+
+def precios_float(precios):
+
+    #transformando a array de valores numericos
+    valores_numericos = []
+    i = 0
+
+    for x in precios:
+        valores_numericos[i] = float(x)
+        i = i+1
+
+    return valores_numericos
+
+def asignar_horarios(valores_numericos):
+    
+    desv_tipica = round(statistics.stdev(valores_numericos),3)
+    i = 0
+    horario = []
+
+    for x in valores_numericos:
+        if x <= desv_tipica:
+            horario[i] = True
+        else: horario[i] = False
+        i = i+1
+    
+    return horario
+
+def arreglar_horarios(horario):
+    i = 1
+    for x in range(1, horario-2):
+        if x == 0:
+            if horario[i-1] == 1 and horario[i+1] == 1:
+                horario[i] = 1
+    
+    return horario
+
+
+def ejecutar_comandos(horario):
+    i = 0
+    comando = ''
+    for x in horario:
+        if x:
+            comando = 'echo \"sh encender.sh\" | at ' + i + ':00'
+            call(comando, shell=True)
+        
         else:
-            if puntero > 0:
-                if buena_hora[puntero - 1]: #si la anterior hora entraba en mi intervalo, la resta se hara de una forma
-                    dif = abs(precio[puntero] - precio[puntero-1]) #diferencia entre hora anterior y actual
-                    if dif < 0.00105: #para no apagar y encender por pequeña diferencia entre horas consecutivas, marco un limite mayor
-                        buena_hora[puntero] = True #si entro, es una hora valida tambien
-                    else:
-                        buena_hora[puntero] = False #hora no valida, el ordenador se apagaría
-                else: #si no, entonces se hara al reves
-                    dif = abs(precio[puntero-1] - precio[puntero])
-                    if dif < 0.00105:
-                        buena_hora[puntero] = True
-                    else:
-                        buena_hora[puntero] = False
-            else:
-                buena_hora[puntero] = False #dado que es la primera hora, tampoco nos vamos a morir por tenerlo apagado un poco mas
-        puntero += 1
-    
-    puntero = 0 #vamos a usar otra vez esta variable, para recorrer lista buena_hora, asi que la reinicializamos
-    
-    for hora in buena_hora:
-        if not hora:
-            if puntero > 0:
-                if hora[puntero-1] == True & hora[puntero+1] == True: #si las horas contiguas son validas, hacemos tambien valida la del medio
-                    hora[puntero] = True
-        else:
-            if puntero > 0:
-                if hora[puntero-1] == False & hora[puntero+1] == False: #si las horas contiguas son malas, hacemos tambien mala la del medio
-                    hora[puntero] = False
-        puntero += 1
+            comando = 'echo \"sh apagar.sh\" | at ' + i + ':00'
+            call(comando, shell=True)
 
-    puntero = 0 #otra vez reutilizamos
+# ------------------- Main ------------------ #
+
+datos_json = obtener_precios()
+
+if len(datos_json) == 0: #error al procesar la peticion, json no valido (vacio)
+    sys.exit(-1) 
     
-    while(True):
-        valor = 0
-        if puntero == 24:
-            break
-        else:
-            if buena_hora[puntero]:
-                valor = puntero
-                while(buena_hora[puntero]):
-                    puntero += 1
-                intervalo.append((valor,puntero-1))
-            else:
-                puntero += 1
+precios = calcular_luz(datos_json)
+valores_numericos = precios_float(precios)
 
-    puntero = 0
-    
-    for interv in intervalo:
-        encendido_apagado.append(interv[puntero][0])
-        encendido_apagado.append(interv[puntero][1])
-        puntero+=1
+#obteniendo minimo, maximo y media
+precio_minimo = min(valores_numericos)
+precio_maximo = max(valores_numericos)
+precio_medio = round(statistics.mean(valores_numericos),3)
 
-    ruta = "/var/spool/cron/crontabs"
+horario = asignar_horarios(valores_numericos, precio_medio)
+horario = arreglar_horarios(horario)
 
-    '''
+ejecutar_comandos(horario)
