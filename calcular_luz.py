583007a14c4e49d85eb360b46915c835c022619b
Antonio Mateo <91947321+fluctlights@users.noreply.github.com>
2023-04-21 22:44:48 +0200

    Mejorado calcular_luz.py

--- calcular_luz.py
+++ calcular_luz.py
@@ -7,32 +7,35 @@ from datetime import datetime
 import requests
 import json
 import statistics
+import subprocess
 
 # ------------------ Variables globales --------------------- #
 
 TOKEN = "" #Token de acceso
 ID_PENINSULA = 8741 #Para filtrar por los precios de la Peninusula
 URL = 'https://api.esios.ree.es/indicators/1001'
-CABECERAS = {'Accept':'application/json; application/vnd.esios-api-v2+json','Content-Type':'application/json','Host':'api.esios.ree.es','Authorization':'Token token=\"' + TOKEN + '\"'}
-DIRECTORIO_LOGS = '' #ruta de guardado de logs con los precios max/min/med del dia
+CABECERAS = {'Accept':'application/json; application/vnd.esios-api-v1+json','Host':'api.esios.ree.es','Content-Type':'application/json','x-api-key':'\"'+TOKEN+'\"'}
+DIRECTORIO_LOGS = '/home/pi/api_ree_logs/' #ruta de guardado de logs con los precios max/min/med del dia
+
 
 # ------------------------ Metodos -------------------------- #
 
 def obtener_datos():
+    try :
+        response = requests.get(URL, headers=CABECERAS)
+        datos_json = {}
+        if response.status_code == 200:  #conexion correcta
+            datos_json = json.loads(response.text) #almacenamos respuesta en formato JSON
+            return datos_json
+    except Exception as e:
+        print(str(e))
+        return datos_json #si tenemos error salimos directamente
 
-    response = requests.get(URL, headers=CABECERAS)
 
-    if response.status_code == 200:  #conexion correcta
-        datos_json = json.loads(response.text) #almacenamos respuesta en formato JSON
-        return datos_json
-    
-    return datos_json #si tenemos error salimos directamente
-
-
-def calcular_precios(datos_json):
+def calcular_precios(datos):
     
         #Para filtrar por valores, y solo aquellos que sean de la Peninsula
-        valores = datos_json['indicator']['values']
+        valores = datos['indicator']['values']
         valores_peninsula = [x for x in valores if x['geo_id'] == ID_PENINSULA]
 
         #Recogiendo precios
@@ -53,11 +56,10 @@ def calcular_precios(datos_json):
 
 def asignar_horarios(precios):
     
-    desv_tipica = round(statistics.stdev(precios), 4)
     horario = []
 
     for x in precios:
-        if x <= desv_tipica:
+        if x <= 0.10:
             horario.append(True)
         
         else: horario.append(False)
@@ -66,38 +68,53 @@ def asignar_horarios(precios):
     # y a la hora siguiente se encenderá también, limitando desgaste
     # de los componentes
 
-    i = 1
-    for x in range(1, len(horario)-2):
-        if x == 0:
-            if horario[i-1] == 1 and horario[i+1] == 1:
-                horario[i] = 1
+    for x in range(0, len(horario)):
+        
+        if (horario[x] == False):
+            
+            if x is 0:
+                if (horario[x+1] == True) and (horario[x+2] == True):
+                    subset = [precios[x+1], precios[x], precios[x+2]]
+                    if round(statistics.mean(subset), 4) <= 0.1150:
+                        horario[x] = True
+                        
+            elif x is (len(horario)-1):
+                if (horario[x-1] == True) and (horario[x-2] == True):
+                    subset = [precios[x-1], precios[x], precios[x-2]]
+                    if round(statistics.mean(subset), 4) <= 0.1150:
+                        horario[x] = True
+                        
+            else:
+                if (horario[x-1] == True) and (horario[x+1] == True):
+                    subset = [precios[x-1], precios[x], precios[x+1]]
+                    if round(statistics.mean(subset), 4) <= 0.1150:
+                        horario[x] = True
     
     return horario
 
 
 def crear_tareas(horario):
     
-    i = 0
-    for x in horario:
-        if x:
-            comando = 'echo \"sh encender.sh\" | at ' + str(i) + ':00 >/dev/null 2>&1 &'
-            call(comando, shell=True)
+    for x in range (0, len(horario)):
+        if horario[x]:
+            #comando = 'echo \"sudo /bin/bash encender.sh\" | at ' + str(x) + ':02 >/dev/null 2>&1 &'
+            comando = 'echo \"sudo /bin/bash encender.sh\" | at ' + str(x) + ':02'
+            subprocess.Popen(comando, shell=True)
         
         else:
-            comando = 'echo \"sh apagar.sh\" | at ' + str(i) + ':00 >/dev/null 2>&1 &'
-            call(comando, shell=True)
+            #comando = 'echo \"/bin/bash apagar.sh\" | at ' + str(x) + ':02 >/dev/null 2>&1 &'
+            comando = 'echo \"/bin/bash apagar.sh\" | at ' + str(x) + ':02'
+            subprocess.Popen(comando, shell=True)
         
-        i = i+1 #siguiente hora
-
-
 def main():
 
-    datos_json = obtener_datos() #conectando con api
+    datos = obtener_datos() #conectando con api
 
-    if len(datos_json) == 0: #error al procesar la peticion, json no valido (vacio)
+    if len(datos) == 0: #error al procesar la peticion, json no valido (vacio)
+        print("err")
         sys.exit(-1) 
         
-    precios = calcular_precios(datos_json) #filtrado de precios y valores en kWh
+    precios = calcular_precios(datos) #precios y valores en kWh
     horario = asignar_horarios(precios) #segun el precio de cada hora, encender/apagar servidor
     crear_tareas(horario) #creacion de tareas de encendido/apagado para cada hora
     return 0
